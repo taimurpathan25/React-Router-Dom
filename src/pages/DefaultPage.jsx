165766ac131c8660417c9ab9d7300ed0c1da2c1a
Pathan <taimurpathan25@gmail.com>
2024-12-30 17:11:07 +0530

    Update the 404 Page with some changes

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Pages.css';
-import { useRouteError } from 'react-router-dom';
+import { NavLink, useRouteError } from 'react-router-dom';
 
 const DefaultPage = () => {
   console.log(useRouteError())
   return (
    <>
    <section className="page_404">
-    <div classNameName="container">
+    <div className="container">
 		<div className="row">	
 		<div className="col-sm-12 ">
 		{/* <div className="col-sm-10 col-sm-offset-1  text-center"> */}
 		<div className="four_zero_four_bg">
 			<img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif" alt="404 Notfound Image" />
-		
 		</div>
 		
 		<div className="contant_box_404 text-center">
@@ -23,7 +22,7 @@ const DefaultPage = () => {
 		
 		<p>the page you are looking for not avaible!</p>
 		
-		<a href="" className="link_404">Go to Home</a>
+		<NavLink to='/' className="link_404">Go to Home</NavLink>
 	</div>
 		{/* </div> */}
 		</div>
